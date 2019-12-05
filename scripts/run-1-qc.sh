c60f6bcfc909bdec50fc769691fc39fdea1d8ccb
M Zulcinski <ummz@login1.arc4.leeds.ac.uk>
2019-12-05 16:54:53 +0000

    finished first step: quality control

--- scripts/run-1-qc.sh
+++ scripts/run-1-qc.sh
@@ -1,51 +1,29 @@
 
-################################################################################# 
-# this script runs first QC analysis (on raw fatsq files) for all files at once
-#################################################################################
+###################################################################################################### 
+# this script runs first QC analysis (on raw fatsq files) for all files in parallel (using array task)
+######################################################################################################
 
-if [ $# != 2 ] ; then
-    echo -e "ERROR: 2 arguments are required: (1) Path to data folder and (2) path to output folder ... Exiting"
+if [ $# != 3 ] ; then
+    echo -e "ERROR: 3 arguments are required: (1) Path to data folder, (2) path to output folder and (3) SGE_TASK_ID argument for array jobs ... Exiting"
     exit 1
 fi	
-
-# TODO: add count to see how many fastq files are in the directoey and to track their processing
-
 
 # define arguments
 data_dir=$1			# path to the folder with fastq files
 out_dir=$2			# path the the folder where output will be placed
 
 # export software (FastQC)
-export PATH=/nobackup/leedsomics_workshop/tools/FastQC:$PATH
+export PATH=/nobackup/ummz/tools/FastQC:$PATH
 
 # check if output folders exist and create them if needed
 if [ ! -d "$out_dir/report" ]; then
     echo -ne "\nCreating directories..."
     mkdir $out_dir/report $out_dir/temp
-    echo -e $out_dir/report
-    echo -e $out_dir/temp
-else
-    read -p "Output directory already exists! Overwrite it? (y|n)? " choice
-    case "$choice" in
-    y|Y ) echo -e "\nOverwrote output directory."; rm -r $out_dir/report $out_dir/temp; mkdir $out_dir/report $out_dir/temp; echo -e $out_dir/report; echo -e $out_dir/temp;;   
-    n|N ) echo -e "\nExiting..."; exit 1;;
-    * )   echo "invalid"; exit 1;;
-    esac
 fi
  
+# get the fastq.gz files names (no need to get read1 and read2 separately)
+fastqFile=$(ls $data_dir/*_001.fastq.gz | sed -n -e "$SGE_TASK_ID p")
 
-for filename in $(ls $data_dir/*.fastq.gz) 
-do
-    fastqc -o $out_dir/report --threads 4 --dir $out_dir/temp $filename
-done
-
-
-
-
-
-
-
-
-
-
+# run the fastqc command for each fastq.gz file
+echo "fastqc -o $out_dir/report --threads 4 --dir $out_dir/temp $fastqFile"
 
--- submission_files/submit-1-qc.sh
+++ submission_files/submit-1-qc.sh
@@ -1,24 +1,12 @@
-# submission script for run_trimm_1.sh
-# Michal Zulcinski 2019-11-13
+# submission script for run-1-qc.sh
+# Michal Zulcinski 2019-12-05
 
-# Run from the current directory with current environment
 #$ -cwd -V
-
-# ask for some time (hh:mm:ss max of 48:00:00)
-#$ -l h_rt=8:00:00
-
-# ask for some memory (by default, 1G, without a request)
-#$ -l h_vmem=1.5G
-
-# ask for 1 core
-#$ -l np=1
-
-# send emails when job starts and ends
+#$ -l h_rt=01:00:00
+#$ -l h_vmem=1G
+#$ -t 1-82
 #$ -m be
-#$ -M @leeds.ac.uk
+#$ -M ummz-arc-records@outlook.com
 
-# now run the job
-./run_trimm_1.sh
-
-# probably need to redirect outpus to a new file
+/nobackup/ummz/analysis_nov19/RNA-seq/scripts/run-1-qc.sh /nobackup/ummz/analysis_nov19/data /nobackup/ummz/analysis_nov19/RNA-seq/results ${SGE_TASK_ID} >> output.$JOB_ID.txt
 
