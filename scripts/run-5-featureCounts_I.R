061592b448d64f30b7ef3655f2a24c583b242820
mihaux <michalzulcinski@gmail.com>
2020-03-10 11:49:24 +0000

    Update and rename run-5-featureCounts.R to run-5-featureCounts_I.R

--- scripts/run-5-featureCounts_I.R
+++ scripts/run-5-featureCounts_I.R
@@ -1,5 +1,7 @@
 ###################################################################################################### 
 # this script runs featureCounts to count reads from STAR output (name_Aligned.sortedByCoord.out.bam)
+# using the array concept with the 'SGE_TASK_ID' parameter
+# so it launches all files (samples) in parallel and produces one output (.csv) file per each sample 
 ###################################################################################################### 
 
 # INPUT:
