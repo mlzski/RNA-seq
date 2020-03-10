12f6733c5b44732d5b782aa5b15f9dd4e0753428
mihaux <michalzulcinski@gmail.com>
2020-03-10 14:04:09 +0000

    Update run-5-featureCounts_II.R

--- scripts/run-5-featureCounts_II.R
+++ scripts/run-5-featureCounts_II.R
@@ -22,7 +22,7 @@ args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args)!=3) {
   stop("3 arguments must be supplied: 
-	\n(1 - input) path to directory with data, 
+	\n(1) running mode parameter [SE or PE], 
 	\n(2 - input) path to directory with data, 
 	\n(3 - output) path where output files should be stored", call.=FALSE)
 }
@@ -35,30 +35,32 @@ cat(args[3], sep="\n")
 
 setwd(args[3])
 
-
 # retrive built-in annotation file (other genomes available: mm9, mm10, hg19 and hg38 (NCBI RefSeq))
 ann <- getInBuiltAnnotation(annotation = "hg38")
 
 # create a list with file names to be processed:
-#files_list <- list.files(args[1], pattern = ".bam$", full.names = TRUE)
-  
-files_list <- c("/nobackup/ummz/analyses/run_I_Nov19/4_alignment/bam/11026_S12_L005_Aligned.sortedByCoord.out.bam", 
-				"/nobackup/ummz/analyses/run_I_Nov19/4_alignment/bam/11028_S4_L005_Aligned.sortedByCoord.out.bam")
+files_list <- list.files(args[2], pattern = ".bam$", full.names = TRUE)
 
+# run featureCounts() command [SE - single end | PE - paired end] AND
+# write output tables with counts and statistics
+if(args[1] == "SE"){
+	fc_SE <- featureCounts(files_list, annot.ext=ann, nthreads = 40)
+	write.csv(fc_SE$counts, file="all_counts_SE.csv")
+	write.csv(fc_SE$stat, file="all_stats_SE.csv")
+} else if (args[1] == "PE"){
+	fc_PE <- featureCounts(files_list, annot.ext=ann, isPairedEnd=TRUE, nthreads = 40)
+	write.csv(fc_PE$counts, file="all_counts_PE.csv")
+	write.csv(fc_PE$stat, file="all_stats_PE.csv")
+} else {
+	stop("ERROR: running mode parameter must be defined as either SE or PE", call.=FALSE)	
+}
 
-fc_SE <- featureCounts(files_list, annot.ext=ann, nthreads = 2) 
-
-#fc_SE <- featureCounts(files_list, annot.ext=ann, nthreads = 1)
-
-# for paired-end (the bam file needs to ogrinate from paired-end mode of alignment)
-# fc_PE <- featureCounts("alignResultsPE.BAM", annot.ext=ann, isPairedEnd=TRUE)
-
-# OUTPUTS:
+###################################################################################################### 
+# featureCounts() OUTPUTS:
 # fc_SE$counts      => a very long vector with counts for all genes
 # fc_SE$annotation  => a table with all genes (rows) 6 columns for: "GeneID", "Chr", "Start", "End", "Strand", "Length"
 # fc_SE$targets     => file name(s) used
 # fc_SE$stat        => a table with statistics (14 metrics)
-
 
 # return fractions of A, T, G and C bases at each base location of reads or in the entire dataset.
 # agtc_cont <- atgcContent(files_list)
@@ -66,14 +68,6 @@ fc_SE <- featureCounts(files_list, annot.ext=ann, nthreads = 2)
 # return the proportion of mapped reads included in a SAM/BAM file. 
 # For paired end reads, it can return the proportion of mapped fragments (ie. read pairs).
 # prop_mapped <- propmapped(files_list)
-
-# write output table with counts
-
-write.csv(fc_SE$counts, file="all_ounts.csv")
-
-#write.csv(fc_SE$counts, file="counts.csv")
-#write.csv(fc_SE$stat, file="stats.csv")
-
 
 # Extract quality strings and convert them to Phred scores for generating boxplots.
 # Quality scores give the probabilities of read bases being incorrectly called
@@ -84,8 +78,4 @@ write.csv(fc_SE$counts, file="all_ounts.csv")
 # input_format = a character string specifying format of the input file. gzFASTQ (gzipped FASTQ) by default. Acceptable formats include gzFASTQ, FASTQ, SAM and BAM. Character string is case insensitive.
 # offset = a numeric value giving the offset added to the base-calling Phred scores. Possible values include 33 and 64. By default, 33 is used.
 # nreads = a numeric value giving the number of reads from which quality scores are extracted. 10000 by default. A value of -1 indicates that quality scores will be extracted from all the reads.
-
-
-
-
 
