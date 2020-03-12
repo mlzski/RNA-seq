d91341e8a903de2506ac96318617c5ee0fce5046
mihaux <michalzulcinski@gmail.com>
2020-03-12 15:49:30 +0000

    Update submit-2-trim.sh

--- submission_files/submit-2-trim.sh
+++ submission_files/submit-2-trim.sh
@@ -10,3 +10,9 @@
 #$ -M ummz-arc-records@outlook.com
 
 /path/to/running/script/run-2-trim.sh /path/to/data /path/to/results ${SGE_TASK_ID} >> /path/to/arc_files/output.$JOB_ID.txt
+
+# for single-end
+# /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/single-end/run-2-trim.sh /nobackup/ummz/analyses/data /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/single-end/ ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/single-end/arc_files/output.$JOB_ID.txt
+
+# for paired-end
+# /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/run-2-trim.sh /nobackup/ummz/analyses/data /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/ ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/arc_files/output.$JOB_ID.txt
