bb290bc0d24bb708f7c25dc4b7765046d2e81e95
mihaux <michalzulcinski@gmail.com>
2020-07-16 15:47:11 +0200

    Update run-4-align.sh

--- scripts/run-4-align.sh
+++ scripts/run-4-align.sh
@@ -5,7 +5,7 @@
 
 if [ $# != 5 ] ; then
     echo -e "ERROR: 5 arguments are required: \n
-    (1) running mode (either 'SE', 'PE-paired' or 'PE-unpaired'), \n
+    (1) running mode (either 'SE' or 'PE'), \n
     (2) path to data folder, \n
     (3) path to output directory, \n
     (4) path to index file and \n
@@ -18,11 +18,10 @@ fi
 export PATH=/home/home02/ummz/tools/STAR-2.7.3a/bin/Linux_x86_64_static:$PATH
 
 # assign variables
-run_mode=$1               # 'SE', 'PE-paired' or 'PE-unpaired'
+run_mode=$1               # 'SE' or 'PE'
 data_dir=$2
 out_dir=$3
 index_dir=$4
-
 
 if [ $run_mode == 'SE' ] ; then
 # get the read1 fastq.gz file
@@ -41,32 +40,14 @@ STAR \
 --outSAMattributes All \
 --outSAMstrandField intronMotif
 
-elif [ $run_mode == 'PE-paired' ] ; then
+elif [ $run_mode == 'PE' ] ; then
 # get the read1 fastq.gz file and its pair
 fastqFile=$(ls $data_dir/*_R1_paired.fq | sed -n -e "$SGE_TASK_ID p")
 read1=$fastqFile
 read2=$(echo $read1 | sed 's/R1/R2/g')
 bam_name=$(ls $data_dir/*_R1_paired.fq | rev | cut -d '/' -f 1 | cut -c 13- | rev | sed -n -e "$SGE_TASK_ID p")
 
-# run STAR alignment in paired-end mode [PE-paired]
-STAR \
---runMode alignReads \
---genomeDir $index_dir \
---runThreadN 8 \
---readFilesIn $read1 $read2 \
---outFileNamePrefix $out_dir/bam/${bam_name} \
---outSAMtype BAM SortedByCoordinate \
---outSAMattributes All \
---outSAMstrandField intronMotif
-
-elif [ $run_mode == 'PE-unpaired' ] ; then
-# get the read1 fastq.gz file and its pair
-fastqFile=$(ls $data_dir/*_R1_unpaired.fq | sed -n -e "$SGE_TASK_ID p")
-read1=$fastqFile
-read2=$(echo $read1 | sed 's/R1/R2/g')
-bam_name=$(ls $data_dir/*_R1_unpaired.fq | rev | cut -d '/' -f 1 | cut -c 13- | rev | sed -n -e "$SGE_TASK_ID p")
-
-# run STAR alignment in paired-end mode [PE-unpaired]
+# run STAR alignment in paired-end mode [PE]
 STAR \
 --runMode alignReads \
 --genomeDir $index_dir \
@@ -78,5 +59,5 @@ STAR \
 --outSAMstrandField intronMotif
 
 else
-echo ERROR... run_mode argument must be specified as: 'SE', 'PE-paired' or 'PE-unpaired'
+echo ERROR... run_mode argument must be specified as: 'SE' or 'PE'
 fi
