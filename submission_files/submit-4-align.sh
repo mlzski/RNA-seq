1fac0712089acf31feb030a3a03f80de0d897904
michal <ummz@login2.arc4.leeds.ac.uk>
2020-07-16 14:53:43 +0100

    modified submission file

--- submission_files/submit-4-align.sh
+++ submission_files/submit-4-align.sh
@@ -1,5 +1,5 @@
 # submission script for run-4-align.sh
-# Michal Zulcinski 2019-12-12
+# Michal Zulcinski 2020-07-15
 
 #$ -cwd -V
 #$ -l h_rt=04:00:00
@@ -9,13 +9,18 @@
 #$ -m be
 #$ -M ummz-arc-records@outlook.com
 
-/path/to/running/script/run-4-align.sh /path/to/input/files /path/to/results /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /path/to/arc_files/output.$JOB_ID.txt
+#/path/to/running/script/run-4-align.sh [SE or PE] /path/to/input/files /path/to/results /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /path/to/arc_files/output.$JOB_ID.txt
 
+#    (1) running mode (either 'SE' or 'PE'), \n
+#    (2) path to data folder, \n
+#    (3) path to output directory, \n
+#    (4) path to index file and \n
+#    (5) SGE_TASK_ID argument for array jobs \n
+
+# examples:
 # for single-end
-# /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end/run-4-align_SE.sh /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/single-end/processed_fastq /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end/arc_files/output.$JOB_ID.txt
+# /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end/run-4-align.sh 'SE' /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/single-end/processed_fastq /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/single-end/arc_files/output.$JOB_ID.txt
 
-# for paired-end | paired
-# /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired/run-4-align_PE_paired.sh /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/processed_fastq/paired /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired/arc_files/output.$JOB_ID.txt
+# for paired-end 
+# /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired/run-4-align.sh 'PE' /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/processed_fastq/paired /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/paired/arc_files/output.$JOB_ID.txt
 
-# for paired-end | unpaired (THERE'S NO POINT OF RUNNING THIS, AS ONE OF THE READS IS EMPTY)
-# /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/unpaired/run-4-align_PE_unpaired.sh /nobackup/ummz/analyses/run_IV_Feb20/2_trimming/paired-end/processed_fastq/unpaired /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/unpaired /nobackup/ummz/reference/index ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_IV_Feb20/4_alignment/paired-end/unpaired/arc_files/output.$JOB_ID.txt
