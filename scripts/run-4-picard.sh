c224a35c6e035e8a3c42c8983ca16e0a2a29f9b1
mihaux <michalzulcinski@gmail.com>
2020-07-17 03:28:16 +0200

    Update run-4-picard.sh

--- scripts/run-4-picard.sh
+++ scripts/run-4-picard.sh
@@ -2,6 +2,8 @@
 # this script runs Picard Tools to locate and tag duplicate reads in a BAM or SAM file
 ######################################################################################################
 
+# need to create a new directory within /bam and place there all _Aligned.sortedByCoord.out.bam
+
 if [ $# != 3 ] ; then
     echo -e "ERROR: 3 arguments are required: \
 	(1) path to data folder [BAM files], \
