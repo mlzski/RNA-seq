f95f20b5baf62edda9c49f76a977cae7a78e61b7
mihaux <michalzulcinski@gmail.com>
2020-08-07 18:26:19 +0200

    Update submit-4-picard.sh

--- submission_files/submit-4-picard.sh
+++ submission_files/submit-4-picard.sh
@@ -1,5 +1,5 @@
 # submission script for run-4-picard.sh
-# Michal Zulcinski 2020-05-17
+# Michal Zulcinski 2020-08-07
 
 #$ -cwd -V
 #$ -l h_rt=04:00:00
@@ -9,5 +9,12 @@
 #$ -m be
 #$ -M ummz-arc-records@outlook.com
 
-/nobackup/ummz/analyses/rerun_Ian/rerun_5/scripts/run-4-picard.sh /nobackup/ummz/analyses/rerun_Ian/rerun_5/bam/outputs /nobackup/ummz/analyses/rerun_Ian/rerun_5/pic ${SGE_TASK_ID} >> /nobackup/ummz/analyses/rerun_Ian/rerun_5/output.$JOB_ID.txt
+/home/home02/ummz/github_dirs/RNA-seq/scripts/run-4-picard.sh /nobackup/ummz/analyses/run_NEW/4_alignemnt/single-end/bam/outputs /nobackup/ummz/analyses/run_NEW/4_picard/single-end ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_NEW/4_picard/single-end/arc_files/output.$JOB_ID.txt
+/home/home02/ummz/github_dirs/RNA-seq/scripts/run-4-picard.sh /nobackup/ummz/analyses/run_NEW/4_alignemnt/paired-end/bam/outputs /nobackup/ummz/analyses/run_NEW/4_picard/paired-end ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_NEW/4_picard/paired-end/arc_files/output.$JOB_ID.txt
 
+# (0) /home/home02/ummz/github_dirs/RNA-seq/scripts/run-4-picard.sh
+# (1) /path/to/data [BAM files]
+# (2) /path/to/results 
+# (3) ${SGE_TASK_ID} 
+# >> 
+# (OUTPUT) /path/to/arc_files/output.$JOB_ID.txt
