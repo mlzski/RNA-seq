1bffc50b26934dc0c0854e438899e8f4a70203e8
mihaux <michalzulcinski@gmail.com>
2020-08-08 00:14:43 +0200

    Update submit-2-trim.sh

--- submission_files/submit-2-trim.sh
+++ submission_files/submit-2-trim.sh
@@ -9,10 +9,11 @@
 #$ -m be
 #$ -M ummz-arc-records@outlook.com
 
-/home/home02/ummz/github_dirs/RNA-seq/scripts/run-2-trim.sh PE /nobackup/ummz/analyses/data /nobackup/ummz/analyses/run_NEW/2_trimming/paired-end/ ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_NEW/2_trimming/paired-end/arc_files/output.$JOB_ID.txt
+/home/home02/ummz/github_dirs/RNA-seq/scripts/run-2-trim.sh 'SE' /nobackup/ummz/analyses/data /nobackup/ummz/analyses/run_12_Aug20/2_trimming_SE ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_12_Aug20/2_trimming_SE/arc_files/output.$JOB_ID.txt
+/home/home02/ummz/github_dirs/RNA-seq/scripts/run-2-trim.sh 'PE' /nobackup/ummz/analyses/data /nobackup/ummz/analyses/run_12_Aug20/2_trimming_PE ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_12_Aug20/2_trimming_PE/arc_files/output.$JOB_ID.txt
 
 # (0) /home/home02/ummz/github_dirs/RNA-seq/scripts/run-2-trim.sh
-# (1) [either SE or PE]
+# (1) [either 'SE' or 'PE']
 # (2) /path/to/data 
 # (3) /path/to/results 
 # (4) ${SGE_TASK_ID} 
