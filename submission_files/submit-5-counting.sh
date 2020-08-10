4be5ece63e59b3cf437b1e1b209793c7d5c20ff6
michal <ummz@login1.arc4.leeds.ac.uk>
2020-08-10 01:55:32 +0100

    created submission file

--- submission_files/submit-5-counting.sh
+++ submission_files/submit-5-counting.sh
@@ -5,32 +5,31 @@
 #$ -l h_rt=04:00:00
 #$ -l h_vmem=2G
 #$ -pe smp 8
-#$ -t 1-2
 #$ -m be
 #$ -M ummz-arc-records@outlook.com
 
-# Rscript /home/home02/ummz/RNA-seq/scripts/run-5-featureCounts_I.R /nobackup/ummz/analyses/run_I_Nov19/4_aligment/bam /nobackup/ummz/analyses/run_I_Nov19/5_featureCounts/postprocessed ${SGE_TASK_ID} >> /nobackup/ummz/analyses/run_I_Nov19/5_featureCounts/arc_files/output.$JOB_ID.txt
+Rscript /home/home02/ummz/RNA-seq/scripts/run-5-counting.R dups SE /nobackup/ummz/analyses/run_12_Aug20/4_picard_SE_all /nobackup/ummz/reference_genome/generatedBySTAR_July20/annotation/hg38.ncbiRefSeq.gtf /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all >> /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all/arc_files/output.$JOB_ID.txt
 
-#Rscript /home/home02/ummz/RNA-seq/scripts/run-5-featureCounts_Ian.R dups SE /nobackup/ummz/analyses/rerun_FINAL/run_1/alignment_SE/bam/outputs /nobackup/ummz/reference_genome/NEW/annotation/hg38.refGene.gtf /nobackup/ummz/analyses/rerun_FINAL/run_1/featCounts_SE >> /nobackup/ummz/analyses/rerun_FINAL/run_1/arc_files/SE/output.$JOB_ID.txt
-#Rscript /home/home02/ummz/RNA-seq/scripts/run-5-featureCounts_Ian.R nodups SE /nobackup/ummz/analyses/rerun_FINAL/run_1/alignment_SE/bam/outputs /nobackup/ummz/reference_genome/NEW/annotation/hg38.refGene.gtf /nobackup/ummz/analyses/rerun_FINAL/run_1/featCounts_SE >> /nobackup/ummz/analyses/rerun_FINAL/run_1/arc_files/SE/output.$JOB_ID.txt
+Rscript /home/home02/ummz/RNA-seq/scripts/run-5-counting.R dups SE /nobackup/ummz/analyses/run_12_Aug20/4_picard_SE_all /n
+    obackup/ummz/reference_genome/generatedBySTAR_July20/annotation/hg38.ncbiRefSeq.gtf /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all >> /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all/arc_files/output.$JOB_ID.txt
 
-#Rscript /home/home02/ummz/RNA-seq/scripts/run-5-featureCounts_Ian.R dups SE /nobackup/ummz/analyses/rerun_FINAL/run_2/alignment_SE/bam/outputs /nobackup/ummz/reference_genome/NEW/annotation/hg38.refGene.gtf /nobackup/ummz/analyses/rerun_FINAL/run_2/featCounts_SE >> /nobackup/ummz/analyses/rerun_FINAL/run_2/arc_files/SE/output.$JOB_ID.txt
-#Rscript /home/home02/ummz/RNA-seq/scripts/run-5-featureCounts_Ian.R nodups SE /nobackup/ummz/analyses/rerun_FINAL/run_2/alignment_SE/bam/outputs /nobackup/ummz/reference_genome/NEW/annotation/hg38.refGene.gtf /nobackup/ummz/analyses/rerun_FINAL/run_2/featCounts_SE >> /nobackup/ummz/analyses/rerun_FINAL/run_2/arc_files/SE/output.$JOB_ID.txt
+Rscript /home/home02/ummz/RNA-seq/scripts/run-5-counting.R dups SE /nobackup/ummz/analyses/run_12_Aug20/4_picard_SE_all /n
+    obackup/ummz/reference_genome/generatedBySTAR_July20/annotation/hg38.ncbiRefSeq.gtf /nobackup/ummz/analyses/run_12_Aug20/5
+    _counting_SE_all >> /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all/arc_files/output.$JOB_ID.txt
 
-# this line needs to be modified every time:  #$ -t 1-2
+Rscript /home/home02/ummz/RNA-seq/scripts/run-5-counting.R dups SE /nobackup/ummz/analyses/run_12_Aug20/4_picard_SE_all /n
+    obackup/ummz/reference_genome/generatedBySTAR_July20/annotation/hg38.ncbiRefSeq.gtf /nobackup/ummz/analyses/run_12_Aug20/5
+    _counting_SE_all >> /nobackup/ummz/analyses/run_12_Aug20/5_counting_SE_all/arc_files/output.$JOB_ID.txt
+
+
 # this line was different in Ian's script:    #$ -l h_rt=04:00:00
 
 # (0) /home/home02/ummz/github_dirs/RNA-seq/scripts/run-5-counting.sh
-# (1) ??? /path/to/data 
-# (2) ??? /path/to/results 
-# (3) ??? ${SGE_TASK_ID} 
+# (1) running option [dups or nodups]
+# (2) running mode parameter [SE or PE]
+# (3) /path/to/input/data [bam files obtained from Picard] 
+# (4) path to GTF annotation files [hg38.ncbiRefSeq.gtf]
+# (5) /path/to/results 
 # >> 
 # (OUTPUT) /path/to/arc_files/output.$JOB_ID.txt
 
-
-
-  	\n(1) running option [dups or nodups],
-	\n(2) running mode parameter [SE or PE],
-	\n(3 - input) path to directory with data, 
-	\n(4 - annotation) path to GTF annotation files and
-	\n(5 - output) path where output files should be stored", call.=FALSE)
