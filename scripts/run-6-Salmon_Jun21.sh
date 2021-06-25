6d8269668201832cfd54d960d4f0fdc8e0b930b0
mihaux <michalzulcinski@gmail.com>
2021-06-26 00:10:41 +0200

    Update run-6-Salmon_Jun21.sh

--- scripts/run-6-Salmon_Jun21.sh
+++ scripts/run-6-Salmon_Jun21.sh
@@ -3,13 +3,16 @@
 
 # TODO: finish implementation for single-end data
 
+# this version is based on this tutorial: https://github.com/crazyhottommy/RNA-seq-analysis/blob/master/salmon_kalliso_STAR_compare.md
+
 if [ $# != 5 ] ; then
-    echo -e "ERROR: 5 arguments are required: \
+    echo -e "ERROR: 6 arguments are required: \
     (1) running mode (either 'SE' or 'PE'), \
     (2) path to folder with input data, \
     (3) path to folder for output directory, \
-    (4) path to index file and \
-    (5) SGE_TASK_ID argument for array jobs ...Exiting"
+    (4) path to index file \
+    (5) path to .gtf file and \
+    (6) SGE_TASK_ID argument for array jobs ...Exiting"
     exit 1
 fi
 
@@ -17,10 +20,11 @@ fi
 export PATH=/home/home02/ummz/tools/salmon-latest_linux_x86_64/bin:$PATH
 
 # assign variables
-run_mode=$1               # 'SE' or 'PE'
+run_mode=$1               # 'transcript-level' or 'gene-level'
 data_dir=$2
 out_dir=$3
 index_dir=$4
+gtf_dir=$5
 
 # prepare params.txt filr with information about this run
 me=$(basename "$0")
@@ -30,25 +34,25 @@ current_date_time="`date "+%Y-%m-%d %H:%M:%S"`";
 echo -ne "Executed on:" $current_date_time "\n" >> params_${me}_.txt
 
 # run Salmon mapping job
-if [ $run_mode == 'SE' ] ; then
-    # get the read1 fastq.gz file
-    fastqFile=$(ls $data_dir/*_R1_single.fq | sed -n -e "$SGE_TASK_ID p")
+if [ $run_mode == 'transcript-level' ] ; then
+   
+   # get the read1 fastq.gz file and its pair read2
+    fastqFile=$(ls $data_dir/*_R1_paired.fq | sed -n -e "$SGE_TASK_ID p")
     read1=$fastqFile
+    read2=$(echo $read1 | sed 's/R1/R2/g')
+    samp_name=$(ls $data_dir/*_R1_paired.fq | rev | cut -d '/' -f 1 | cut -c 13- | rev | sed -n -e "$SGE_TASK_ID p")
+
+    # run Salmon in paired-end mode [PE]
+    salmon quant -i $index_dir -l ISR -1 $read1 -2 $read2 -p 8 -o $out_dir/${samp_name}_quant 
     
-    samp_name=$(ls $data_dir/*_R1_single.fq | rev | cut -d '/' -f 1 | cut -c 13- | rev | sed -n -e "$SGE_TASK_ID p")
+    # NOTE: use either "-l ISR" or "-l A" (automatic); for new data, always "A"
+ 
+    # print the main command to params.txt
+    echo "salmon quant -i $index_dir -l ISR -1 $read1 -2 $read2 -p 8 -o $out_dir/${samp_name}_quant" >> params_${me}_.txt
 
-    # run Salmon in single-end mode [SE]
-    #STAR \	[TODO: TO BE COMPLETED]
-    #--runMode alignReads \
-    #--genomeDir $index_dir \
-    #--runThreadN 8 \
-    #--readFilesIn $read1 \
-    #--outFileNamePrefix $out_dir/bam/${bam_name} \
-    #--outSAMtype BAM SortedByCoordinate \
-    #--outSAMunmapped Within \
-    #--outSAMattributes Standard 
 
-elif [ $run_mode == 'PE' ] ; then
+elif [ $run_mode == 'gene-level' ] ; then
+
     # get the read1 fastq.gz file and its pair read2
     fastqFile=$(ls $data_dir/*_R1_paired.fq | sed -n -e "$SGE_TASK_ID p")
     read1=$fastqFile
@@ -56,22 +60,19 @@ elif [ $run_mode == 'PE' ] ; then
     samp_name=$(ls $data_dir/*_R1_paired.fq | rev | cut -d '/' -f 1 | cut -c 13- | rev | sed -n -e "$SGE_TASK_ID p")
 
     # run Salmon in paired-end mode [PE]
-    salmon quant -i $index_dir -l A -1 $read1 -2 $read2 -p 8 -o $out_dir/${samp_name}_quant --gcBias --numBootstraps 100
-    
-    # print the main command to params.txt
-    echo "salmon quant -i $index_dir -l A -1 $read1 -2 $read2 -p 8 -o $out_dir/${samp_name}_quant --gcBias --numBootstraps 100" >> params_${me}_.txt
+    salmon quant -i $index_dir -l ISR -1 $read1 -2 $read2 -p 8 -o $out_dir/${samp_name}_quant -g $gtf_dir --fldMean 200 --fldSD 20
 
-#salmon quant -i /n/groups/hbctraining/ngs-data-analysis-longcourse/rnaseq/salmon.ensembl38.idx \
-# -l A \
-# -r $fq \
-# -p 6 \
-# -o $base.salmon \
-# --seqBias \
-# --useVBOpt \
-# --numBootstraps 30
+    # NOTE: use either "-l ISR" or "-l A" (automatic); for new data, always "A"
+ 
+    # print the main command to params.txt
+    echo " " >> params_${me}_.txt
+
+# NOTES:
+# --fldMean     => specify the fragment length to be 200 bp (default)
+# --fldSD       => specify standard deviation of 20 (default 80)
 
 else
-    echo "ERROR... run_mode argument must be specified as: 'SE' or 'PE'"
+    echo "ERROR... run_mode argument must be specified as: 'transcript-level' or 'gene-level'"
     exit 1
 fi
 
