d7025cd543dcea22b693f5aefa77a02070fb3f6d
michal <ummz@login1.arc4.leeds.ac.uk>
2021-06-27 22:31:58 +0100

    updated submission file

---
+++ submission_files/submit_getCounts_swish.sh
@@ -0,0 +1,20 @@
+# submission script to run get_counts_swish.R to obtain count matrix on gene- and transcript-level
+# Michal Zulcinski 2021-04-15
+
+#$ -cwd -V
+#$ -l h_rt=01:00:00
+#$ -l h_vmem=32G
+#$ -m be
+#$ -M ummz-arc-records@outlook.com
+
+Rscript /home/home02/ummz/github_dirs/RNA-seq/scripts/get_counts_swish.R /nobackup/ummz/analyses/run_17_Jun21/quants_all/transcript-level /nobackup/ummz/analyses/run_17_Jun21/swish/transcript-level /nobackup/ummz/analyses/run_17_Jun21/swish/samples_all.txt
+
+#Rscript /home/home02/ummz/github_dirs/RNA-seq/scripts/get_counts_swish.R /nobackup/ummz/analyses/run_17_Jun21/quants_all/gene-level /nobackup/ummz/analyses/run_17_Jun21/swish/gene-level /nobackup/ummz/analyses/run_17_Jun21/swish/samples_all.txt
+
+#############################################################
+# Rscript run_DEG_swish_FINAL.R
+# (1) INPUT directory
+# (2) OUTPUT directory
+# (3) annotation file [samples.txt]
+
+
