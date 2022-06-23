013f66dc382622ffbc275a121e90f4a66d722b6b
michal <ummz@login2.arc4.leeds.ac.uk>
2022-06-23 14:24:09 +0100

    updated submission script for Salmon

--- submission_files/submit-Salmon.sh
+++ submission_files/submit-Salmon.sh
@@ -9,16 +9,16 @@
 #$ -l h_rt=08:00:00
 #$ -l h_vmem=9G
 #$ -pe smp 8
-#$ -t 1-2
+#$ -t 1-91
 #$ -j y
 #$ -N read-quantif
 #$ -m be
 #$ -M ummz-arc-records@outlook.com
 
 # define arguments to be passed
-script=/home/home02/ummz/github_dirs/RNA-seq/scripts/run-Salmon.sh
-infile=/nobackup/ummz/analysis-May-22/2-trim-compressed/test
-outfile=/nobackup/ummz/analysis-May-22/read-quantification/output
+script=/nobackup/ummz/analysis-May-22/read-quantification/run-Salmon.sh
+infile=/nobackup/ummz/analysis-May-22/2-trim-compressed/trimmed/paired-end/paired
+outfile=/nobackup/ummz/analysis-May-22/read-quantification/output-quantif
 index=/nobackup/ummz/analysis-May-22/reference-genome/salmon_index
 gtf=/nobackup/ummz/analysis-May-22/reference-genome/gencode.v40.annotation.gtf.gz
 
@@ -26,7 +26,7 @@ gtf=/nobackup/ummz/analysis-May-22/reference-genome/gencode.v40.annotation.gtf.g
 $script 'transcript-level' $infile $outfile $index $gtf ${SGE_TASK_ID} >> /nobackup/ummz/analysis-May-22/read-quantification/arc_files/output-transcript-level.$JOB_ID.txt
 
 # running on gene level
-$script 'gene-level' $infile $outfile $index $gtf ${SGE_TASK_ID} >> /nobackup/ummz/analysis-May-22/read-quantification/arc_files/output-gene-level.$JOB_ID.txt
+### $script 'gene-level' $infile $outfile $index $gtf ${SGE_TASK_ID} >> /nobackup/ummz/analysis-May-22/read-quantification/arc_files/output-gene-level.$JOB_ID.txt
 
 ###################################################################
 # (0) /home/home02/ummz/github_dirs/RNA-seq/scripts/run-Salmon.sh
